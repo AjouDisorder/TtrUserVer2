2bc42d98f507efd4db789416f1a4169930509eb5
ijpsm9 <ijpsm9@icloud.com>
2020-05-15 03:24:23 +0900

    navigation 적용 끝

--- app/src/main/java/com/example/ttruserver2/MainActivity.kt
+++ app/src/main/java/com/example/ttruserver2/MainActivity.kt
@@ -5,10 +5,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.viewpager.widget.ViewPager
 import com.example.ttruserver.ViewPagerAdapter
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
 //import retrofit2.Call
 //import retrofit2.Callback
 //import retrofit2.Response
@@ -16,7 +25,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 //import retrofit2.converter.gson.GsonConverterFactory
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var toolbar: Toolbar //toolbar is androidx.appcompat.widget
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
 
     internal lateinit var viewpager : ViewPager
 
@@ -24,7 +37,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//navigation
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, 0, 0
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+//navigation
         val img = arrayOf(
             R.drawable.ai,
             R.drawable.css,
@@ -106,5 +132,40 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = ViewPagerAdapter(this)
         viewpager.adapter = adapter
+    }
+
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when (p0.itemId) {
+            R.id.navigation_home-> {
+                Toast.makeText(this, "navigation_home clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.navigation_bike -> {
+                Toast.makeText(this, "navigation_bike clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.navigation_bus -> {
+                Toast.makeText(this, "navigation_bus clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.navigation_airplane -> {
+                Toast.makeText(this, "navigation_airplane clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.navigation_login -> {
+                Toast.makeText(this, "navigation_login clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.navigation_profile -> {
+                Toast.makeText(this, "navigation_profile clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.navigation_logout -> {
+                Toast.makeText(this, "navigation_logout clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.navigation_cloud -> {
+                Toast.makeText(this, "navigation_cloud clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.navigation_wifi -> {
+                Toast.makeText(this, "navigation_wifi clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
